a6cb2f0d8e84e27f418eab21d0de1418e4583b3b
marlon tunes <smarlon6@gmail.com>
2024-02-25 11:33:53 -0400

    listar_postagem

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts_app.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Adicionar Isto
--- posts_app/views.py
+++ posts_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from posts_app.models import Posts
 
 # Create your views here.
+def post_list(request):
+    template_name = 'post-list.html' # template
+    posts = Posts.objects.all() # query com todas as postagens
+    context = { # cria context para chamar no template
+        'posts': posts
+        }
+    return render(request, template_name, context) # render
